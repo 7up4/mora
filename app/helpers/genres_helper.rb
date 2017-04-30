de0e865db8f8224ff11db7996571a9d5bd2c393c
Nurlan Musayev <nurlancik1@gmail.com>
2017-04-30 19:37:41 +0300

    Some DRY

--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,2 +1,11 @@
 module GenresHelper
+  def change_a_field(id)
+    capture_haml do
+      if id =~ /\A[^aeiou]/
+        haml_concat  link_to "Change a #{id.gsub(/_/, ' ')}", "#", class: "hide_field", id: "#{id}"
+      else
+        haml_concat  link_to "Change an #{id.gsub(/_/, ' ')}", "#", class: "hide_field", id: "#{id}"
+      end
+    end
+  end
 end
